498098928f692e8297a8f55695d7198b8bfec64c
yihanliux <yihanliu3@163.com>
2025-05-05 14:16:15 +0800

    更改

--- static/analysis.js
+++ static/analysis.js
@@ -30,47 +30,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 document.getElementById('result-container1').style.display = 'flex';
                 document.getElementById('result-container2').style.display = 'flex';
 
-
-                // const segment1 = data.Segment1;
-                // document.getElementById("icon1").innerHTML = segment1;
-                // const segment2 = data.Segment2;
-                // document.getElementById("result-text1").innerHTML = segment2;
-                // const segment3 = data.Segment3;
-                // document.getElementById("icon2").innerHTML = segment3;
-                // const segment4 = data.Segment4;
-                // document.getElementById("result-text2").innerHTML = segment4;
-
-                // 更新图片
-                // const resultImage_1 = document.getElementById("result-image-1");
-                // resultImage_1.src = data.up_image;
-                // resultImage_1.style.display = 'block';
-
-                // const leftImage = document.getElementById("result-image-2");
-                // leftImage.src = data.left_image;
-                // leftImage.style.display = 'block';
-
-                // const rightImage = document.getElementById("result-image-3");
-                // rightImage.src = data.right_image;
-                // rightImage.style.display = 'block';
-
-
-            //     const resultImage1 = document.getElementById("suggestion-image-1");
-            //     if (data.image_url_1 != null) {  // 确认 image_url_2 不是 None 或 null
-            //         resultImage1.src = data.image_url1;
-            //         resultImage1.style.display = 'block';
-            //     } else {
-            //         resultImage1.style.display = 'none';  // 如果是 None 或 null，则不展示图片
-            //     }
-
-            //     const resultImage2 = document.getElementById("suggestion-image-2");
-            //     if (data.image_url_2 != null) {  // 确认 image_url_2 不是 None 或 null
-            //         resultImage2.src = data.image_url2;
-            //         resultImage2.style.display = 'block';
-            //     } else {
-            //         resultImage2.style.display = 'none';  // 如果是 None 或 null，则不展示图片
-            //     }
-            // } else {
-            //     setTimeout(checkStatus, 2000);
             }
         });
     }
